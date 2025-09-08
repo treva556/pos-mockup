41648a60e32dac86d05848c630c60225f538de7b
treva556 <marktreva92@gmail.com>
2025-09-09 00:03:06 +0300

    comt

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -7,8 +7,12 @@ function Products() {
   return (
     <div>
         Products Page
-
+        <div className=' flex-col'>
         <h1> All Products</h1>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+         Add Product
+        </button>
+         </div>
         <ProductComponent/>
     </div>
   );
