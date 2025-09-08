fb9518310034f4d90748dccc16af71d6ebc5e4bc
treva556 <marktreva92@gmail.com>
2025-09-08 17:41:41 +0300

    comt

---
+++ src/components/Navlink.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+     
+
+     <h1 className="text-3xl font-bold underline">
+      Hello world!
+     </h1>
+    </div>
+  );
+}
+
+export default App;
+
+
