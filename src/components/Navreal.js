3ddb98e085baef06b19ace966c2bf62568a2ccdf
treva556 <marktreva92@gmail.com>
2026-04-07 05:04:27 +0300

    suppliers nav update

--- src/components/Navreal.js
+++ src/components/Navreal.js
@@ -140,7 +140,7 @@ export default function Example() {
 
               </MenuItems>
               </Menu>     
-                  
+
               {/* CUSTOMERS MENU
               <Menu as="div" className="relative inline-block text-left">
                 <MenuButton className="inline-flex items-center rounded-md px-3 py-2 text-sm font-medium text-gray-200 hover:bg-white/5 hover:text-white">
@@ -158,8 +158,47 @@ export default function Example() {
                 </MenuItems>
               </Menu> */}
 
-              {/* SUPPLIERS MENU */}
+             {/* SUPPLIER MENU */}
               <Menu as="div" className="relative inline-block text-left">
+                <MenuButton className="inline-flex items-center rounded-md px-3 py-2 text-sm font-medium text-gray-200 hover:bg-white/5 hover:text-white">
+                  Suppliers
+                  <ChevronDownIcon aria-hidden="true" className="ml-1 h-4 w-4 text-gray-300" />
+                </MenuButton>
+                <MenuItems className="absolute left-0 z-10 mt-2 w-44 origin-top-left rounded-md bg-gray-800 py-1 outline -outline-offset-1 outline-white/10">
+
+                <MenuItem as={Link}
+                  to="/suppliers"
+                  className="block px-4 py-2 text-sm text-gray-300 hover:bg-white/5 hover:text-white"
+                >
+                  Supplier Center
+                </MenuItem>
+
+                <MenuItem as={Link}
+                  to="/bills"
+                  className="block px-4 py-2 text-sm text-gray-300 hover:bg-white/5 hover:text-white"
+                >
+                 Bills
+                </MenuItem>
+
+                <MenuItem as={Link}
+                  to="/purchase_orders"
+                  className="block px-4 py-2 text-sm text-gray-300 hover:bg-white/5 hover:text-white"
+                >
+                 Purchase Orders
+                </MenuItem>
+
+               
+              </MenuItems>
+              </Menu>     
+
+
+
+
+
+
+
+              {/* SUPPLIERS MENU */}
+              {/* <Menu as="div" className="relative inline-block text-left">
                 <MenuButton className="inline-flex items-center rounded-md px-3 py-2 text-sm font-medium text-gray-200 hover:bg-white/5 hover:text-white">
                   Suppliers
                   <ChevronDownIcon aria-hidden="true" className="ml-1 h-4 w-4 text-gray-300" />
@@ -173,7 +212,17 @@ export default function Example() {
                     </MenuItem>
                   ))}
                 </MenuItems>
-              </Menu>
+              </Menu> */}
+
+
+
+
+
+
+
+
+
+
 
                 {/* BANKING MENU */}
               <Menu as="div" className="relative inline-block text-left">
