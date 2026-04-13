e9650d5820eb7509dba7c03ea1716ee3d5dc48bc
treva556 <marktreva92@gmail.com>
2026-04-14 00:57:01 +0300

    users section

--- src/pageGroups/users/Users.js
+++ src/pageGroups/users/Users.js
@@ -10,21 +10,21 @@ function Users() {
             name: "John Doe",
             email: "something@gmail.com",
             role: "ADMIN",
-          
+            phone: "(+254) 0713703389",
           },
           {
             id: 2,
             name: "Mark Trevor",
             email: "something@gmail.com",
             role: "CASHIER",
-           
+            phone: "(+254) 0713703389",
           },
           {
             id: 3,
             name: "Bruce Wayne",
             email: "nothing@gmail.com",
             role: "CASHIER",
-           
+            phone: "(+254) 0713703389",
           },
         ];
       
@@ -38,8 +38,9 @@ function Users() {
             {/* Header */}
             <div className="mb-6">
               <h2 className="text-xl font-semibold text-gray-800">
-                User Center : Only ADMINS can create & see users
+                User Center 
               </h2>
+              <h3 className="text-lg font-semibold text-purple-700 "> : Only ADMINS can create & see users</h3>
              <button className="absolute border-purple-400 border-2 mr-10 rounded-md right-0 flex items-center"> 
                 <h3 className=" m-2"> add user</h3>
               </button>
@@ -48,7 +49,7 @@ function Users() {
             {/* Table */}
             <div className="overflow-x-auto mt-5">
               <table className="min-w-full border border-gray-200 rounded-lg mt-5">
-                <thead className="bg-purple-300 text-left text-sm text-gray-600">
+                <thead className="bg-purple-300 text-left text-sm text-white">
                   <tr>
                     <th className="px-4 py-3">NAME</th>
                     <th className="px-4 py-3">EMAIL</th>
@@ -62,6 +63,7 @@ function Users() {
                       <td className="px-4 py-3">{item.name}</td>
                       <td className="px-4 py-3">{item.email}</td>
                       <td className="px-4 py-3">{item.role}</td>
+                      <td className="px-4 py-3">{item.phone}</td>
                       <td className="px-4 py-3 font-medium text-gray-900">
                         {calculateStockValue(item)}
                       </td>
